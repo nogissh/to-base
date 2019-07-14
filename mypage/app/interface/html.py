384d9a7c9cb38f4358d70e24b5650e57b523e473
Toshiki Ohnogi <thetoshiki0419@gmail.com>
2019-07-15 00:33:09 +0900

    update: Jinja2ベースの設計

--- mypage/app/interface/html.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-
-def sigmus_2018_wajima(request):
-  return render(request, 'interface/documents/sigmus_2018_wajima.html')
-
-
-def master_experiment(request):
-  return render(request, 'interface/documents/master_experiment.html')
